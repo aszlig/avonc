c0fe95decfc53b181d64dad61bdeddf15c28dcec
aszlig <aszlig@nix.build>
2019-03-06 04:17:53 +0100

    Move enableDisableApps into a dedicated file
    The nextcloud.nix file gets quite long and we're going to make changes
    on the app enabler/disabler to better handle changed settings. Adding
    more code to that tool within Nix code would make that even worse, so
    let's move it into a new "tools" directory.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- nextcloud.nix
+++ nextcloud.nix
@@ -304,51 +304,12 @@ let
       appconf = lib.genAttrs appPart.right (app: cfg.apps.${app}.config);
     }));
 
-    enableDisableApps = pkgs.writeScript "nextcloud-enable-disable-apps.py" ''
-      #!${pkgs.python3Packages.python.interpreter}
-      import json
-      import subprocess
-      import sys
+    enableDisableApps = lib.escapeShellArgs [
+      pkgs.python3Packages.python.interpreter
+      "${tools/enable-disable-apps.py}"
+    ];
 
-      occ_cmd = sys.argv[2:]
-      cmd = occ_cmd + ['app:list', '--output=json']
-      applist = subprocess.check_output(cmd)
-      oldstate = json.loads(applist)
-      newstate = json.load(open(sys.argv[1], 'r'))
-
-      if 'enable' in newstate:
-        newenabled = set(newstate['enable'].keys()) \
-                   - set(oldstate['enabled'].keys())
-
-        group_deferred = {}
-
-        for appid in newenabled:
-          groups = newstate['enable'][appid]
-          if groups is not None:
-            group_deferred[appid] = groups
-            continue
-          subprocess.check_call(occ_cmd + ['app:enable', appid])
-
-        for appid, groups in group_deferred.items():
-          groupargs = [arg for group in groups for arg in ['-g', group]]
-          subprocess.check_call(occ_cmd + ['app:enable'] + groupargs + [appid])
-
-        for appid in newenabled:
-          for key, value in newstate.get('appconf', {}).get(appid, {}).items():
-            args = ['config:app:set', appid, key, '--value=' + value]
-            subprocess.check_call(occ_cmd + args)
-
-      newdisabled = set(newstate['disable']) \
-                  - set(oldstate['disabled'].keys())
-
-      for appid in newdisabled:
-        if appid not in oldstate['enabled'] and \
-           appid not in oldstate['disabled']:
-          continue
-        subprocess.check_call(occ_cmd + ['app:disable', appid])
-    '';
-
-  in "${lib.escapeShellArg enableDisableApps} ${newState} ${occCmd}";
+  in "${enableDisableApps} ${lib.escapeShellArg newState} ${occCmd}";
 
   nextcloudInit = pkgs.runCommand "nextcloud-init" {
     nativeBuildInputs = [
