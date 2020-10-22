4a2779632edc9a150dfd736a73a2bc73200bffcb
aszlig <aszlig@nix.build>
2020-10-22 18:51:25 +0200

    tests/upgrade: Switch to using nesting.clone
    While nesting.clone got renamed to "specialisation", which is a better
    name and also better interface (since it uses an attribute set instead
    of a list), we can't yet use it since we're currently pinning NixOS
    20.03.
    
    Of course, we could run the tests using NixOS 20.09, but since
    everything else is NixOS 20.03 the test would be less useful since it
    won't detect breakages in NixOS 20.03.
    
    Nevertheless, using nesting.clone already is a much better interface for
    handling machine generations since we no longer need to rely on
    internals of NixOS VM tests, nor do we need to add dummy machines.
    
    The implementation can still be improved to provide a more sequential
    datatype and allowing multiple major version upgrades, but I'll defer
    that cleanup to after we switched to "specialisation" since it doesn't
    make sense to build on an API that's going to be obsolete very soon.
    
    I also removed "ldap_write_support" from the apps to be enabled, since
    it only really works if there is an LDAP provider. If that's not the
    case, the app doesn't even enable and will later result in a mismatch
    when checking enabled apps against the one we expected to be enabled.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- tests/upgrade.nix
+++ tests/upgrade.nix
@@ -1,172 +1,171 @@
 import ./make-test.nix (pkgs: {
   name = "nextcloud-upgrade";
 
-  nodes = let
-    common = { options, pkgs, ... }: {
-      nextcloud.enable = true;
-      nextcloud.domain = "localhost";
+  machine = { config, lib, pkgs, ... }: {
+    nextcloud.enable = true;
+    nextcloud.domain = "localhost";
+    nextcloud.majorVersion = lib.mkDefault 19;
 
-      services.nginx.enable = true;
-      services.postgresql.enable = true;
-      systemd.services.postgresql.environment = {
-        LD_PRELOAD = "${pkgs.libeatmydata}/lib/libeatmydata.so";
-      };
-
-      environment.systemPackages = [ pkgs.jq ];
-      virtualisation.memorySize = 1024;
+    services.nginx.enable = true;
+    services.postgresql.enable = true;
+    systemd.services.postgresql.environment = {
+      LD_PRELOAD = "${pkgs.libeatmydata}/lib/libeatmydata.so";
     };
 
-  in {
-    generation1 = { lib, options, config, pkgs, ... }: {
-      imports = [ common ];
+    virtualisation.memorySize = 1024;
 
-      nextcloud.majorVersion = 19;
-
-      nextcloud.apps = let
-        nc19apps = (lib.importJSON ../packages/19/upstream.json).applications;
-
-        excludedApps = [
-          # We'll need dpendency ordering for this app
-          "auto_mail_accounts"
-          # Needs Perl
-          "camerarawpreviews"
-          # Needs /usr/bin/clamscan
-          "files_antivirus"
-          # Broken upgrade routine across major versions
-          "files_external_dropbox"
-          # Upstream URL very unstable
-          "files_external_gdrive"
-          # Needs PHP's inotify extension
-          "files_inotify"
-          # Needs the OAuth library
-          "grauphel"
-          # Only works with MyQSL
-          "sensorlogger"
-          # Windows only (at least it seems)
-          "sharepoint"
-          # Seems to be incompatible with PostgreSQL
-          "twofactor_admin"
-          # Needs "user_backend_sql_raw" in config
-          "user_backend_sql_raw"
-          # Needs write access to to "apps/cms_pico/appdata_public/".
-          "cms_pico"
-          # No OCC support, since it requires $_SERVER['REQUEST_URI']
-          "sendent"
-          # XXX: Currently (2019-12-12) results in HTTP 404
-          "emlviewer"
-          # XXX: Conflicts with the "news" app - investigate this someday.
-          "files_mindmap"
-          # XXX: Hash mismatch in upstream URL
-          "tencentcloudcosconfig"
-
-          # XXX: Requires pdlib and thus module system integration for PHP
-          #      extensions.
-          "facerecognition"
-
-          # XXX: Requires GnuPG
-          "gpgmailer"
-
-          # We already have a LibreOffice Online build from source, so no need
-          # to test the binary releases:
-          "richdocumentscode"
-          "richdocumentscode_arm64"
-
-          # https://github.com/nextcloud/officeonline/pull/6
-          (assert nc19apps.officeonline.version == "1.0.0"; "officeonline")
-
-          # https://github.com/nextcloud/user_oidc/issues/90
-          (assert nc19apps.user_oidc.version == "0.2.1"; "user_oidc")
-
-          # https://github.com/e-alfred/epubreader/issues/22
-          (assert nc19apps.epubreader.version == "1.4.3"; "epubreader")
-
-          # These apps have non-deterministic download URLs
-          "occweb"
-          "quicknotes"
-          "spgverein"
-          "twainwebscan"
-          "twofactor_yubikey"
-
-          # Will be enabled later
-          "spreed"
-          "richdocuments"
-        ];
-        enableApp = name: let
-          isExcluded = lib.elem name excludedApps;
-        in lib.const (lib.optionalAttrs (!isExcluded) {
-          enable = true;
-        });
-      in lib.mapAttrs enableApp nc19apps;
-    };
-
-    generation2 = { config, lib, nodes, ... }: {
-      imports = [ common ];
-
-      systemd.services.libreoffice-online.enable = false;
-      systemd.services.mongooseim.enable = false;
-
-      nextcloud.apps = lib.genAttrs [
-        "apporder" "bookmarks" "calendar" "circles" "contacts" "deck"
-        "external" "end_to_end_encryption" "files_accesscontrol"
-        "files_markdown" "files_rightclick" "gpxpod" "groupfolders" "mail"
-        "metadata" "news" "passwords" "polls" "phonetrack" "richdocuments"
-        # XXX: Temporarily disable "social" app, but attach assertion to
-        #      "spreed" to maximum ugliness (and to possibly trigger OCD).
-        #      Upstream issue: https://github.com/nextcloud/social/issues/1034
-        (let inherit (config.nextcloud.package) applications;
-        in assert applications.social."20".version == "0.4.1"; "spreed")
-        "tasks"
-      ] (lib.const { enable = true; });
-    };
-  };
-
-  testScript = { nodes, ... }: let
-    switchToGeneration = gen: let
-      node = "generation${toString gen}";
-      inherit (nodes.${node}.config.system.build) toplevel;
-      switchCmd = "${toplevel}/bin/switch-to-configuration test";
-    in ''
-      with machine.nested('switch to generation ${toString gen}'):
-        machine.succeed('${switchCmd} >&2')
-    '';
-
-    expectedApps = let
-      inherit (pkgs.lib) attrNames filterAttrs const escape;
-      inherit (nodes.generation2.config.nextcloud) apps;
+    system.extraSystemBuilderCmds = let
+      isEnabled = lib.const (x: x.enable);
+      enabledApps = lib.filterAttrs isEnabled config.nextcloud.apps;
       alwaysEnabled = [
         "cloud_federation_api" "dav" "federatedfilesharing" "files"
         "lookup_server_connector" "oauth2" "provisioning_api" "settings"
         "twofactor_backupcodes" "viewer" "workflowengine"
       ];
-      enabled = attrNames (filterAttrs (const (x: x.enable)) apps);
-    in builtins.toJSON (enabled ++ alwaysEnabled);
+      json = builtins.toJSON (lib.attrNames enabledApps ++ alwaysEnabled);
+    in "echo ${lib.escapeShellArg json} > \"$out/nc-enabled-apps.json\"";
+
+    nesting.clone = let
+      nc19apps = (lib.importJSON ../packages/19/upstream.json).applications;
+
+      excludedApps = [
+        # We'll need dpendency ordering for this app
+        "auto_mail_accounts"
+        # Needs Perl
+        "camerarawpreviews"
+        # Needs /usr/bin/clamscan
+        "files_antivirus"
+        # Broken upgrade routine across major versions
+        "files_external_dropbox"
+        # Upstream URL very unstable
+        "files_external_gdrive"
+        # Needs PHP's inotify extension
+        "files_inotify"
+        # Needs the OAuth library
+        "grauphel"
+        # Needs to have an LDAP provide
+        "ldap_write_support"
+        # Only works with MyQSL
+        "sensorlogger"
+        # Windows only (at least it seems)
+        "sharepoint"
+        # Seems to be incompatible with PostgreSQL
+        "twofactor_admin"
+        # Needs "user_backend_sql_raw" in config
+        "user_backend_sql_raw"
+        # Needs write access to to "apps/cms_pico/appdata_public/".
+        "cms_pico"
+        # No OCC support, since it requires $_SERVER['REQUEST_URI']
+        "sendent"
+        # XXX: Currently (2019-12-12) results in HTTP 404
+        "emlviewer"
+        # XXX: Conflicts with the "news" app - investigate this someday.
+        "files_mindmap"
+        # XXX: Hash mismatch in upstream URL
+        "tencentcloudcosconfig"
+
+        # XXX: Requires pdlib and thus module system integration for PHP
+        #      extensions.
+        "facerecognition"
+
+        # XXX: Requires GnuPG
+        "gpgmailer"
+
+        # We already have a LibreOffice Online build from source, so no need
+        # to test the binary releases:
+        "richdocumentscode"
+        "richdocumentscode_arm64"
+
+        # https://github.com/nextcloud/officeonline/pull/6
+        (assert nc19apps.officeonline.version == "1.0.0"; "officeonline")
+
+        # https://github.com/nextcloud/user_oidc/issues/90
+        (assert nc19apps.user_oidc.version == "0.2.1"; "user_oidc")
+
+        # https://github.com/e-alfred/epubreader/issues/22
+        (assert nc19apps.epubreader.version == "1.4.3"; "epubreader")
+
+        # These apps have non-deterministic download URLs
+        "occweb"
+        "quicknotes"
+        "spgverein"
+        "twainwebscan"
+        "twofactor_yubikey"
+
+        # Will be enabled later
+        "spreed"
+        "richdocuments"
+      ];
+
+      enableApp = name: let
+        isExcluded = lib.elem name excludedApps;
+      in lib.const (lib.optionalAttrs (!isExcluded) {
+        enable = true;
+      });
+
+    in [
+      { nextcloud.majorVersion = 19;
+        nextcloud.apps = lib.mapAttrs enableApp nc19apps;
+      }
+      { nextcloud.majorVersion = 20;
+        nextcloud.apps = lib.genAttrs [
+          "apporder" "bookmarks" "calendar" "circles" "contacts" "deck"
+          "external" "end_to_end_encryption" "files_accesscontrol"
+          "files_markdown" "files_rightclick" "gpxpod" "groupfolders" "mail"
+          "metadata" "news" "passwords" "polls" "phonetrack" "richdocuments"
+          # XXX: Temporarily disable "social" app, but attach assertion to
+          #      "spreed" to maximum ugliness (and to possibly trigger OCD).
+          # Upstream issue: https://github.com/nextcloud/social/issues/1034
+          (let inherit (config.nextcloud.package) applications;
+          in assert applications.social."20".version == "0.4.1"; "spreed")
+          "tasks"
+        ] (lib.const { enable = true; });
+      }
+    ];
+  };
+
+  testScript = { nodes, ... }: let
+    inherit (nodes.machine.config.system.build) toplevel;
+    getChild = num: "${toplevel}/fine-tune/child-${toString num}";
+
+    checkApps = systemRoot: ''
+      with machine.nested('checking whether enabled apps match expected ones'):
+        applist = machine.succeed('nextcloud-occ app:list --output=json')
+        enabled = json.loads(applist)['enabled'].keys()
+        with open('${systemRoot}/nc-enabled-apps.json', 'r') as fp:
+          expected = set(json.load(fp))
+        assert enabled == expected, f'{enabled} != {expected}'
+    '';
+
+    switchToGeneration = num: desc: ''
+      with machine.nested(r'${pkgs.lib.escape ["'" "\\"] desc}'):
+        machine.succeed('${getChild num}/bin/switch-to-configuration test >&2')
+        machine.start_job('nextcloud.service')
+        machine.wait_for_unit('nextcloud.service')
+
+      ${checkApps (getChild num)}
+    '';
 
   in ''
     # fmt: off
     import json
-    machine = generation1
-
     machine.wait_for_unit('multi-user.target')
 
     machine.start_job('nextcloud.service')
     machine.wait_for_unit('nextcloud.service')
 
+    ${checkApps toplevel}
+
+    ${switchToGeneration 1 "enable as many apps as possible"}
+
     machine.succeed(
       'curl -L http://localhost/ | grep -o "a safe home for all your data"'
     )
 
-    ${switchToGeneration 2}
-    machine.start_job('nextcloud.service')
-    machine.wait_for_unit('nextcloud.service')
+    ${switchToGeneration 2 "switch to new major version"}
 
     machine.succeed(
       'curl -L http://localhost/ | grep -o initial-state-core-loginUsername'
     )
-
-    applist = machine.succeed('nextcloud-occ app:list --output=json')
-    enabled = json.loads(applist)['enabled'].keys()
-
-    expected = set(json.loads(r'${pkgs.lib.escape ["'" "\\"] expectedApps}'))
-    assert expected == enabled, f'{expected} != {enabled}'
   '';
 })
