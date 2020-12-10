53ffc5ec487ce96b5ea2fa11181c0650680dab3f
aszlig <aszlig@nix.build>
2020-12-10 08:46:02 +0100

    tests: Re-add "social" app
    This reverts commit ff621ea4656931678bfe20dbbdac7b480bacc030.
    
    Back then I added an assertion on version 0.4.1 to make sure I'd recheck
    whether a new upstream version of the app works.
    
    So with 9cfb61039dfb05ab328511fedd9d8d0255c7c94b, we introduced the new
    0.4.2 upstream release. Running our upgrade test succeeds with this
    release, so it seems that the original issue[1] is now fixed.
    
    [1]: https://github.com/nextcloud/social/issues/1034
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- tests/upgrade.nix
+++ tests/upgrade.nix
@@ -107,12 +107,7 @@ import ./make-test.nix (pkgs: {
           "external" "end_to_end_encryption" "files_accesscontrol"
           "files_markdown" "files_rightclick" "gpxpod" "groupfolders" "mail"
           "metadata" "news" "passwords" "polls" "phonetrack" "richdocuments"
-          # XXX: Temporarily disable "social" app, but attach assertion to
-          #      "spreed" to maximum ugliness (and to possibly trigger OCD).
-          # Upstream issue: https://github.com/nextcloud/social/issues/1034
-          (let inherit (config.nextcloud.package) applications;
-          in assert applications.social."20".version == "0.4.1"; "spreed")
-          "tasks"
+          "social" "spreed" "tasks"
         ] (lib.const { enable = true; });
       }
     ];
