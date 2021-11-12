316131a4ded2965164d4e6fa8c6c442d4b0b2a15
aszlig <aszlig@nix.build>
2021-11-12 05:32:06 +0100

    Fix readonly-config-upgrade patch for NC 20 + 21
    Upgrading to Nextcloud 21.0.6.2 introduced a small change that causes a
    conflict when applying our patch. The rebase of the patch is rather
    trivial since it only adds the $appManager variable that we don't use
    anyway.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- packages/20/default.nix
+++ packages/20/default.nix
@@ -2,7 +2,7 @@
   patches = [
     patches/no-config-uid-check.patch
     ../19/patches/executable-lookup.patch
-    ../19/patches/readonly-config-upgrade.patch
+    patches/readonly-config-upgrade.patch
     ../19/patches/ipaddr-recognise-unix.patch
   ];
 }
